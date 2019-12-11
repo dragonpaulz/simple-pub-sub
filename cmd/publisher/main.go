969f3b5c4045555faba3a3dfff28b4d63e134aa0
dragonpaulz <paul.geesaman@gmail.com>
2019-12-11 16:19:23 -0500

    Removing unused lines of code

--- cmd/publisher/main.go
+++ cmd/publisher/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"log"
 	"math/rand"
 	"time"
@@ -10,17 +9,12 @@ import (
 )
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-
-	log.Printf("context: %v, cancel: %v", ctx, cancel)
-
 	conn, channel, conf, connErr := config.RedisPubConn()
 	if connErr != nil {
 		log.Fatalf("Cannot establish connection. Exiting.")
 	}
 
 	defer conn.Close()
-	// done := make(chan error, 1)
 
 	sleepTime := time.Second / conf.PerSecond
 	for {
