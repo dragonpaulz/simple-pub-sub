50806fab9ba763eadc0a35a52368c3ddc22e4358
dragonpaulz <paul.geesaman@gmail.com>
2019-12-17 12:26:55 -0500

    Test for receiving two numbers.

--- cmd/subscriber/internal/receive/receive_test.go
+++ cmd/subscriber/internal/receive/receive_test.go
@@ -44,3 +44,30 @@ func TestReceive_mockReceiver(t *testing.T) {
 
 	assert.Equal(t, 1, received)
 }
+
+func TestReceiveMultiple_mockReceiver(t *testing.T) {
+	timeout := time.After(100 * time.Millisecond)
+	testobj := new(mockReceiver)
+
+	testobj.On("Receive").Return(redis.Message{Data: []byte(strconv.Itoa(1))}).Once()
+	testobj.On("Receive").Return(redis.Message{Data: []byte(strconv.Itoa(2))})
+	NumChan := make(chan int)
+	DoneChan := make(chan error)
+
+	go receive.Receive(DoneChan, NumChan, testobj)
+
+	received := make([]int, 2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case n := <-NumChan:
+			received[i] = n
+			fmt.Println("Received ", n)
+		case <-timeout:
+			t.Fatal("Test timed out")
+		}
+	}
+
+	assert.Contains(t, received, 1)
+	assert.Contains(t, received, 2)
+}
